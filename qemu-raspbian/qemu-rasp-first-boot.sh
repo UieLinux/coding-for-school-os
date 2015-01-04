686d0bf1d17823dda6c8dcece9edac64c67a259f
strippat <strippato@gmail.co>
2015-01-04 20:40:08 +0100

    new name for raspbian image file

--- qemu-raspbian/qemu-rasp-first-boot.sh
+++ qemu-raspbian/qemu-rasp-first-boot.sh
@@ -7,6 +7,6 @@ qemu-system-arm \
   -no-reboot \
   -serial stdio \
   -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw init=/bin/bash" \
-  -hda 2014-09-09-wheezy-raspbian.img \
+  -hda 2014-12-24-wheezy-raspbian.img \
   -k it \
   -display sdl
